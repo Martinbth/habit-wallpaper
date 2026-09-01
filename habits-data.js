3e04a341f64d3d89d789990aa22e5f7f902e91b9
Martinbth <Martinbth@users.noreply.github.com>
2026-09-01 06:41:37 +0200

    Update habits from Obsidian

--- habits-data.js
+++ habits-data.js
@@ -32,5 +32,8 @@ window.habitsData = [
 ];
 window.readinessStart = "2026-08-27";
 window.readinessData = {
-  "2026-08-28": 61
+  "2026-08-28": 61,
+  "2026-08-29": 43,
+  "2026-08-30": 31,
+  "2026-08-31": 57
 };
